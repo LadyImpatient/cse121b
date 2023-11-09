1239aa33f388573aed13865830b44073740d1133
LadyImpatient <aliciapuuwai@gmail.com>
2023-11-08 20:31:39 -0600

    code update w2 task

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -1,29 +1,36 @@
 /* W02-Task - Profile Home Page */
 
-/* Step 1 - Setup type tasks - no code required */
-
 /* Step 2 - Variables */
-
-
-
+const fullName = 'Alicia Unga-Dinh';
+const currentYear = new Date().getFullYear(); 
+const profilePicture = 'images/IMG_3700.JPG'; 
 
 /* Step 3 - Element Variables */
-
-
-
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
 
 /* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
 
+yearElement.textContent = currentYear;
 
-
-
-
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`); 
 
 /* Step 5 - Array */
+const favoriteFoods = ['Albondigas', 'Ice cream', 'Sushi', 'Spam fried rice'];
 
+foodElement.innerHTML = favoriteFoods.join('<br>'); 
 
+const newFavoriteFood = 'Mochi butter cake';
+favoriteFoods.push(newFavoriteFood);
 
+foodElement.innerHTML += `<br>${newFavoriteFood}`;
 
+favoriteFoods.shift();
+foodElement.innerHTML = favoriteFoods.join('<br>');
 
-
+favoriteFoods.pop();
+foodElement.innerHTML = favoriteFoods.join('<br>');
