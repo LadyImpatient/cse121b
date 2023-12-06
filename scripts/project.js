5bda1e5aa0510f8552d87542d2fb9b871e3068d9
LadyImpatient <aliciapuuwai@gmail.com>
2023-12-05 22:56:08 -0600

    minor fix

--- scripts/project.js
+++ scripts/project.js
@@ -24,30 +24,32 @@ function fetchAnimalInfo() {
 
 function displayAnimalInfo(animalInfo) {
     var animalInfoContainer = document.getElementById('animalInfoContainer');
-    animalInfoContainer.innerHTML = '';
-
+    animalInfoContainer.innerHTML = ''; // Clear previous content
+  
     // Check if the API returned an error
     if (animalInfo.message) {
-        animalInfoContainer.innerHTML = `<p>${animalInfo.message}</p>`;
-        return;
+      animalInfoContainer.innerHTML = `<p>${animalInfo.message}</p>`;
+      return;
     }
-
+  
+    // Display the first animal's information from the response
     var firstAnimal = animalInfo[0];
-
-    // find the information using array
-    var locations = firstAnimal.locations ? firstAnimal.locations.join(', ') : 'N/A';
-
+  
     var infoHTML = `
-        <h2>${firstAnimal.name}</h2>
-        <p><strong>Scientific Name:</strong> ${firstAnimal.taxonomy.scientific_name || 'N/A'}</p>
-        <p><strong>Category:</strong> ${firstAnimal.characteristics.type || 'N/A'}</p>
-        <p><strong>Family:</strong> ${firstAnimal.taxonomy.family || 'N/A'}</p>
-        <p><strong>Order:</strong> ${firstAnimal.taxonomy.order || 'N/A'}</p>
-        <p><strong>Locations:</strong> ${locations}</p>
-        <p><strong>Main Prey:</strong> ${firstAnimal.characteristics.main_prey || 'N/A'}</p>
-        <p><strong>Distinctive Feature:</strong> ${firstAnimal.characteristics.distinctive_feature || 'N/A'}</p>
-        <p><strong>Temperament:</strong> ${firstAnimal.characteristics.temperament || 'N/A'}</p>
-        <p><strong>Diet:</strong> ${firstAnimal.characteristics.diet || 'N/A'}</p>
-        <p><strong>Average Litter Size:</strong> ${firstAnimal.characteristics.average_litter_size || 'N/A'}</p>
+      <h2>${firstAnimal.name}</h2>
+      <p><strong>Scientific Name:</strong> ${firstAnimal.taxonomy.scientific_name || 'N/A'}</p>
+      <p><strong>Category:</strong> ${firstAnimal.characteristics.type || 'N/A'}</p>
+      <p><strong>Family:</strong> ${firstAnimal.taxonomy.family || 'N/A'}</p>
+      <p><strong>Order:</strong> ${firstAnimal.taxonomy.order || 'N/A'}</p>
+      <p><strong>Locations:</strong> ${firstAnimal.locations.join(', ') || 'N/A'}</p>
+      <p><strong>Main Prey:</strong> ${firstAnimal.characteristics.main_prey || 'N/A'}</p>
+      <p><strong>Distinctive Feature:</strong> ${firstAnimal.characteristics.distinctive_feature || 'N/A'}</p>
+      <p><strong>Temperament:</strong> ${firstAnimal.characteristics.temperament || 'N/A'}</p>
+      <p><strong>Diet:</strong> ${firstAnimal.characteristics.diet || 'N/A'}</p>
+      <p><strong>Average Litter Size:</strong> ${firstAnimal.characteristics.average_litter_size || 'N/A'}</p>
     `;
-}
+  
+    // Append the animal information to the container
+    animalInfoContainer.innerHTML = infoHTML;
+  }
+  
